699f837880210087c7d787c8bbd90d9b2ae10a0e
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2022-02-08 14:54:24 -0800

    Retrieving most common of every POS

--- randomize_jokes.py
+++ randomize_jokes.py
@@ -22,10 +22,10 @@ from django.conf import settings
 from torchtext.data import get_tokenizer
 from fastai.text.all import *
 
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
-from nltk.corpus import wordnet
-from nltk import FreqDist
+# nltk.download('wordnet')
+# nltk.download('averaged_perceptron_tagger')
+# from nltk.corpus import wordnet
+# from nltk import FreqDist
 from string import punctuation
 import mmap
 
@@ -38,6 +38,10 @@ from sklearn.decomposition import PCA
 
 
 path = os.getcwd()
+path_resources = os.path.join(path, 'resources')
+available_nouns = []
+available_verbs = []
+available_adjectives = []
 
 class RandomizeJokes:
     df = None
@@ -48,6 +52,37 @@ class RandomizeJokes:
     learn = None
 
     embeddings = None
+
+    available_nouns = []
+    available_verbs = []
+    available_adjectives = []
+
+
+
+    def main(self):
+        # getting all nouns
+        available_nouns = self.read_file('most-common-nouns-english.csv')
+        available_verbs = self.read_file('most-common-verbs-english.csv')
+        available_adjs = self.read_file('english-adjectives.txt')
+
+        '''
+        print(available_nouns[0:20])
+        print(len(available_nouns))
+        print(available_verbs[0:20])
+        print(len(available_verbs))
+        print(available_adjs[0:20])
+        print(len(available_adjs))
+        '''
+    
+    def read_file(self, file_path):
+        try:
+            df_thisfile = pd.read_csv(os.path.join(path_resources, file_path))
+            return df_thisfile.iloc[0:len(df_thisfile),0]
+        except:
+            print('Could not read file: ' + os.path.join(path, file_path))
+            return []
+
+
 
 r = RandomizeJokes()
 r.main()
